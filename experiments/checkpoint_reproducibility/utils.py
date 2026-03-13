e6a0d9f7fe03930fc804d04d1b266989a2f793c3
Rajat <seriousargenti@gmail.com>
2026-03-13 19:20:28 +0530

    utils ver 1

---
+++ experiments/checkpoint_reproducibility/utils.py
@@ -0,0 +1,20 @@
+import torch
+import random
+import numpy as np
+import hashlib
+
+
+def set_seed(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    torch.use_deterministic_algorithms(True)
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def hash_file(path):
+    with open(path, "rb") as f:
+        return hashlib.sha256(f.read()).hexdigest()
