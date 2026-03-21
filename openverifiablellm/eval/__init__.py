e72d58b6362d679a9bb1dd186dd83dc842877a0f
chaitanyaam <147836528+chaitanyaam@users.noreply.github.com>
2026-03-22 02:01:09 +0530

    implement factual evaluator

--- openverifiablellm/eval/__init__.py
+++ openverifiablellm/eval/__init__.py
@@ -1,7 +1,5 @@
-from .bias import WinoBiasEvaluator
 from .perplexity import PerplexityEvaluator
 
 __all__ = [
-    "WinoBiasEvaluator",
     "PerplexityEvaluator",
 ]
--- openverifiablellm/eval/bias/__init__.py
+++
@@ -1,5 +0,0 @@
-from .wino_bias import WinoBiasEvaluator
-
-__all__ = [
-    "WinoBiasEvaluator",
-]
